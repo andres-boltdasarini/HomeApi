d42643cb8292aeb827c8b035c519ab960b7e2214
Андрей <lau47@mail.ru>
2025-06-29 17:48:13 +0300

    valid_attr

--- HomeApi/Contracts/Device/AddDeviceRequest.cs
+++ HomeApi/Contracts/Device/AddDeviceRequest.cs
@@ -1,13 +1,22 @@
-﻿namespace HomeApi.Contracts.Device
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HomeApi.Contracts.Device
 {
     public class AddDeviceRequest
     {
+        [Required] // Указываем все параметры как обязательные
         public string Name { get; set; }
+        [Required]
         public string Manufacturer { get; set; }
+        [Required]
         public string Model { get; set; }
+        [Required]
         public string SerialNumber { get; set; }
+        [Required]
         public int CurrentVolts { get; set; }
+        [Required]
         public bool GasUsage { get; set; }
+        [Required]
         public string Location { get; set; }
     }
 }
--- HomeApi/Controllers/DevicesController.cs
+++ HomeApi/Controllers/DevicesController.cs
@@ -35,10 +35,17 @@ namespace HomeApi.Controllers
         [HttpPost]
         [Route("Add")]
         public IActionResult Add(
-          [FromBody] // Атрибут, указывающий, откуда брать значение объекта
+           [FromBody] // Атрибут, указывающий, откуда биндить значение объекта
    AddDeviceRequest request // Объект запроса
         )
         {
+            if (request.CurrentVolts < 120)
+            {
+                // Добавляем для клиента информативную ошибку
+                ModelState.AddModelError("currentVolts", "Устройства с напряжением меньше 120 вольт не поддерживаются!");
+                return BadRequest(ModelState);
+            }
+
             return StatusCode(200, $"Устройство {request.Name} добавлено!");
         }
     }
