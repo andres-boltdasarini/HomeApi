f442111504421d500fbfdd1b5a962622d743f758
Андрей <lau47@mail.ru>
2025-06-29 13:01:40 +0300

    AddDeviceRequest_post

---
+++ HomeApi/Controllers/DevicesController.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using HomeApi.Configuration;
+using HomeApi.Contracts.Device;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace HomeApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DevicesController : ControllerBase
+    {
+        private IOptions<HomeOptions> _options;
+        private IMapper _mapper;
+
+        public DevicesController(IOptions<HomeOptions> options, IMapper mapper)
+        {
+            _options = options;
+            _mapper = mapper;
+        }
+
+        /// <summary>
+        /// Просмотр списка подключенных устройств
+        /// </summary>
+        [HttpGet]
+        [Route("")]
+        public IActionResult Get()
+        {
+            return StatusCode(200, "Устройства отсутствуют");
+        }
+
+        /// <summary>
+        /// Добавление нового устройства
+        /// </summary>
+        [HttpPost]
+        [Route("Add")]
+        public IActionResult Add(
+          [FromBody] // Атрибут, указывающий, откуда брать значение объекта
+   AddDeviceRequest request // Объект запроса
+        )
+        {
+            return StatusCode(200, $"Устройство {request.Name} добавлено!");
+        }
+    }
+}
